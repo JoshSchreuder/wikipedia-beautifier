c9aca727f7f69d481a30ffd7baa2d23dbef6653f
Josh Schreuder <joshschreuder@gmail.com>
2011-03-23 16:01:28 +1100

    Make Wikipedia super-minimal (removing nav)

--- wikipedia-beautifier.js
+++ wikipedia-beautifier.js
@@ -2,24 +2,48 @@ function wiknifier()
 {
     var fonts = '"minion-pro-1", "minion-pro-2", Palatino, Georgia, "Times New Roman", serif';
 
+    var navColumn = document.getElementById("mw-panel");
+    var header = document.getElementById("mw-head");
+    var headBase = document.getElementById("mw-head-base");
+    var pageBase = document.getElementById("mw-page-base");
+    navColumn.parentNode.removeChild(navColumn);
+    pageBase.parentNode.removeChild(pageBase);
+    header.parentNode.removeChild(header);
+    headBase.parentNode.removeChild(headBase);
+
     var firstHeading = document.getElementById("firstHeading");
     firstHeading.style.fontFamily = fonts;
     firstHeading.style.width = "900px";
-    firstHeading.style.marginLeft = firstHeading.style.marginRight = "auto";
+    firstHeading.style.marginLeft = "5em"; //  = firstHeading.style.marginRight
     firstHeading.style.paddingTop = firstHeading.style.paddingBottom = "0.25em";
     firstHeading.style.border = "none";
     firstHeading.style.fontWeight = "bold";
+
+    var contentBox = document.getElementById("content");
+    contentBox.style.position = "relative";
+    contentBox.style.margin = contentBox.style.marginTop = contentBox.style.marginRight = contentBox.style.marginBottom = contentBox.style.marginLeft = "0px";
 
     var bodyContent = document.getElementById("bodyContent");
     bodyContent.style.lineHeight = "1.55em";
     bodyContent.style.fontSize = "1.0em";
     bodyContent.style.fontFamily = fonts;
     bodyContent.style.border = "1px solid #ccc";
-    bodyContent.style.width = "800px";
-    bodyContent.style.padding = "50px";
-    bodyContent.style.marginLeft = bodyContent.style.marginRight = "auto";
-    bodyContent.style.marginTop = "1em";
+    bodyContent.style.width = "1000px";
+    bodyContent.style.padding = bodyContent.style.paddingTop = bodyContent.style.paddingRight = bodyContent.style.paddingBottom = bodyContent.style.paddingLeft = "15px";
+    bodyContent.style.marginLeft  = "7em"; //= bodyContent.style.marginRight
     bodyContent.style.background = "#fbfbfb";
+
+    var wikiTables = document.getElementsByClassName("wikitable");
+    for(var i = 0; i < wikiTables.length; i++)
+    {
+        wikiTables[i].width = "1000px";
+        wikiTables[i].style.lineHeight = "1.35em";
+        wikiTables[i].style.fontSize = "0.9em";
+        wikiTables[i].style.fontFamily = fonts;
+        wikiTables[i].style.border = "1px solid #ccc";
+        wikiTables[i].style.width = "1000px";
+        wikiTables[i].style.padding = wikiTables[i].style.paddingTop = wikiTables[i].style.paddingRight = wikiTables[i].style.paddingBottom = 	wikiTables[i].style.paddingLeft = "5px";
+    }
 
     function parentDiv(element)
     {
@@ -65,59 +89,6 @@ function wiknifier()
     {
         refLists[i].style["-webkit-column-width"] = "380px";
     }
-
-    var increment = 30;
-
-    function fadeIn(element)
-    {
-        if(element.fadeTimeout)
-        {
-            clearTimeout(element.fadeTimeout);
-        }
-
-        if(parseFloat(element.style.opacity) < 1.0)
-        {
-            element.style.opacity =
-                Math.round(10 * Math.min(parseFloat(element.style.opacity) + 0.1, 1.0)) / 10;
-            element.fadeTimeout = setTimeout(function() { fadeIn(element) }, increment);
-        }
-    }
-
-    function fadeOut(element, limit)
-    {
-        if(element.fadeTimeout)
-        {
-            clearTimeout(element.fadeTimeout);
-        }
-
-        if(!limit)
-        {
-            limit = 0;
-        }
-
-        if(parseFloat(element.style.opacity) > limit)
-        {
-            element.style.opacity =
-                Math.round(10 * Math.max(parseFloat(element.style.opacity) - 0.1, limit)) / 10;
-            element.fadeTimeout = setTimeout(function() { fadeOut(element, limit) }, increment);
-        }
-    }
-
-    function fader(element)
-    {
-        var transparent = 0;
-
-        element.style.opacity = transparent;
-        element.onmouseover = function() {
-            fadeIn(element);
-        }
-        element.onmouseout = function() {
-            element.fadeTimeout = setTimeout(function() { fadeOut(element, transparent) }, 100);
-        }
-    }
-
-    fader(document.getElementById("mw-head"));
-    fader(document.getElementById("mw-panel"));
 }
 
 wiknifier();
